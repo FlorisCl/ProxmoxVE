44f38fe1652f3ce0a7c64884a8133cee92783ea7
FlorisLava <floris.claessensmd@gmail.com>
2026-01-12 16:51:26 +0100

    fix: reverted curls back to community-scripts version

--- ct/wger.sh
+++ ct/wger.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/FlorisCl/ProxmoxVE/fix-wger-script/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Original Author: Slaviša Arežina (tremor021)
 # Revamped Script: Floris Claessens (FlorisCl)
@@ -10,7 +10,7 @@ APP="wger"
 var_tags="${var_tags:-management;fitness}"
 var_cpu="${var_cpu:-1}"
 var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-6}"
+var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
 var_unprivileged="${var_unprivileged:-1}"
--- install/wger-install.sh
+++ install/wger-install.sh
@@ -158,14 +158,9 @@ fetch_wger_source() {
 
 
   RELEASE=$(curl -fsSL https://api.github.com/repos/wger-project/wger/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
-  # curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz -o ${RELEASE}.tar.gz
-  # tar xzf ${RELEASE}.tar.gz
-  # mv wger-${RELEASE} ${WGER_SRC}
-
-  # Get refactor-settings branch
-  curl -fsSL https://github.com/wger-project/wger/archive/refs/heads/master.tar.gz -o master.tar.gz
-  tar xzf master.tar.gz
-  mv wger-master ${WGER_SRC}
+  curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz -o ${RELEASE}.tar.gz
+  tar xzf ${RELEASE}.tar.gz
+  mv wger-${RELEASE} ${WGER_SRC}
 
   rm -rf "${temp_dir}"
   echo "${RELEASE}" >/opt/wger_version.txt
