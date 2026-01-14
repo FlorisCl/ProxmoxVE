1ad3a1b5f995ee52b9180868072e9f5e8742a3c9
FlorisLava <floris.claessensmd@gmail.com>
2026-01-14 20:00:07 +0100

    moved to heads main

--- ct/wger.sh
+++ ct/wger.sh
@@ -45,11 +45,13 @@ function update_script() {
  
   msg_info "Downloading version ${RELEASE}"
   temp_file=$(mktemp -d)
-  curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz \
+  # curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz \
+    curl -fsSL https://github.com/wger-project/wger/archive/refs/heads/main.tar.gz \
     | tar xz -C "${temp_file}"
 
   rsync -a --delete \
-    "${temp_file}/wger-${RELEASE}/" "${WGER_SRC}/"
+    "${temp_file}/wger-main/" "${WGER_SRC}/"
+    # "${temp_file}/wger-${RELEASE}/" "${WGER_SRC}/"
   rm -rf "${temp_file}"
   msg_ok "Source updated"
 
