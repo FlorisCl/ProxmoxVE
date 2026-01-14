16a34878c1c5b51cbec540974f92f6f922a55ead
FlorisLava <floris.claessensmd@gmail.com>
2026-01-14 20:11:57 +0100

    added -

--- ct/wger.sh
+++ ct/wger.sh
@@ -45,9 +45,10 @@ function update_script() {
  
   msg_info "Downloading version ${RELEASE}"
   temp_file=$(mktemp -d)
-  curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz \
+  # curl -fsSL https://github.com/wger-project/wger/archive/refs/tags/${RELEASE}.tar.gz \
   curl -fsSL https://github.com/wger-project/wger/archive/refs/heads/master.tar.gz \
-    | tar xz -C "${temp_file}"
+    | tar xz - -C "${temp_file}"
+    # | tar xz -C "${temp_file}"
 
   rsync -a --delete \
     "${temp_file}/wger-${RELEASE}/" "${WGER_SRC}/"
