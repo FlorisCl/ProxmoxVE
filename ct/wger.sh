ccdd3b9c47a947051b8ad7a46efdee34216788f9
FlorisLava <floris.claessensmd@gmail.com>
2026-01-14 19:41:24 +0100

    added env variables for migration script in updater

--- ct/wger.sh
+++ ct/wger.sh
@@ -63,6 +63,10 @@ function update_script() {
 
   cd "${WGER_SRC}" || exit
   
+  export DJANGO_SETTINGS_MODULE=settings.main
+  export PYTHONPATH="${WGER_SRC}"
+  export USE_CELERY=True
+  
   msg_info "Updating Python dependencies"
   $STD "${WGER_VENV}/bin/python" -m pip install -U pip setuptools wheel
   $STD "${WGER_VENV}/bin/python" -m pip install -e .
