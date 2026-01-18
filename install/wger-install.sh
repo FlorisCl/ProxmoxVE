a49622f006a09bdf09c1b62a2afb39b4390ba2b1
Floris Claessens <floris.claessensmd@gmail.com>
2026-01-18 21:46:37 +0100

    removed -u -E

--- install/wger-install.sh
+++ install/wger-install.sh
@@ -55,8 +55,8 @@ msg_info "Setting up wger"
   export DJANGO_SETTINGS_MODULE=settings.main
   export PYTHONPATH=/opt/wger
 
-  $STD -u wger -E /opt/wger/.venv/bin/wger bootstrap
-  $STD -u wger -E /opt/wger/.venv/bin/python manage.py collectstatic --no-input
+  $STD /opt/wger/.venv/bin/wger bootstrap
+  $STD /opt/wger/.venv/bin/python manage.py collectstatic --no-input
 msg_ok "Finished setting up wger"
 
 msg_info "Creating wger service"
